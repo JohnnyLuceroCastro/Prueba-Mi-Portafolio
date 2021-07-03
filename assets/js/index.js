bdb5bd9512cef2ef808820671d37e31a7d717abb
JohnnyLuceroCastro <johnnylucero@outlook.com>
2021-07-02 23:04:13 -0400

    agregado smooth scroll y fijada la Navbar

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $("a").click(function (event) {
+        event.preventDefault();
+
+        var gato = this.hash;
+
+        $("html").animate(
+            {
+            scrollTop: $(gato).offset().top,
+        },
+        800
+        );
+    });
+
+});
